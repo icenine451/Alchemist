c6cbc3ea4020b7e79fd0bdc1887587072f3819b8
icenine451 <benjamin.r.shelton@protonmail.com>
2025-10-24 15:33:30 -0400

    Combine parent source and optional additional_sources into single array of JSON objects for iteration

--- alchemist.sh
+++ alchemist.sh
@@ -24,28 +24,26 @@ transmute() {
   # component_recipe.json file information extraction
   component_recipe_contents=$(jq -r '.' "$component_recipe_file")
   component_name="$(jq -r '. | keys[]' <<< $component_recipe_contents)"
-  component_source_type="$(jq -r '.[].source_type' <<< $component_recipe_contents)"
-  component_source_url="$(jq -r '.[].source_url' <<< $component_recipe_contents)"
-  component_version="$(jq -r '.[].version' <<< $component_recipe_contents)"
-  component_extraction_type="$(jq -r '.[].extraction_type' <<< $component_recipe_contents)"
 
   COMPONENT_ARTIFACT_ROOT="$workdir/$component_name-artifact" # Initialize the final destination for kept files
   export "$COMPONENT_ARTIFACT_ROOT" # Export for use across all stages
 
-  mkdir -p "$COMPONENT_ARTIFACT_ROOT" # Initialize the work area
+  combined_sources_array=$(echo "$component_recipe_contents" | jq -c '
+    .[] as $parent |
+    ([$parent] + ($parent.additional_sources // [])) |
+    map(del(.additional_sources))
+  ')
 
-  download_cli -t "$component_source_type" -u "$component_source_url" -d "$workdir" -v "$component_version"
+  echo "$combined_sources_array" | jq -c '.[]' | while read -r source_obj; do
+    source_type="$(jq -r '.source_type' <<< $source_obj)"
+    source_url="$(jq -r '.source_url' <<< $source_obj)"
+    source_version="$(jq -r '.version' <<< $source_obj)"
 
-  if [[ ! -z "$component_additional_sources" ]]; then # If there are additional source files to download
-    echo "$component_additional_sources" | jq -c '.[]' | while read -r additional_source_obj; do
-      additional_source_type="$(jq -r '.source_type' <<< $additional_source_obj)"
-      additional_source_url="$(jq -r '.source_url' <<< $additional_source_obj)"
-      additional_source_version="$(jq -r '.version' <<< $additional_source_obj)"
-      additional_source_extraction_type="$(jq -r '.extraction_type' <<< $additional_source_obj)"
+    result=$(download_cli -t "$source_type" -u "$source_url" -d "$workdir" -v "$source_version")
+    downloaded_file=$(echo "$result" | grep "^DOWNLOADED_FILE=" | cut -d= -f2)
 
-      download_cli -t "$additional_source_type" -u "$additional_source_url" -d "$workdir" -v "$additional_source_version"
-    done
-  fi
+    source_extraction_type="$(jq -r '.extraction_type' <<< $source_obj)"
+  done
 }
 
 transmute "$@"
