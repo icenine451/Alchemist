3852e797d7704555c0d6dc432ef95a9f7aad0513
icenine451 <benjamin.r.shelton@protonmail.com>
2025-10-29 14:55:59 -0400

    Add support for installing a specific version of a flatpak app

--- lib/downloaders/flatpak_id.sh
+++ lib/downloaders/flatpak_id.sh
@@ -15,13 +15,13 @@ download() {
   local max_delay="${7:-30}"
 
   if [[ "$flatpak_install_mode" == "user" ]]; then
-    final_dest="$FLATPAK_USER_ROOT/$flatpak_id/current/active/files"
+    final_dest="$FLATPAK_USER_ROOT/app/$flatpak_id/current/$flatpak_version/files"
   elif [[ "$flatpak_install_mode" == "system" ]]; then
-    final_dest="$FLATPAK_SYSTEM_ROOT/$flatpak_id/current/active/files"
+    final_dest="$FLATPAK_SYSTEM_ROOT/app/$flatpak_id/current/$flatpak_version/files"
   else
     log warn "Provided Flatpak destination invalid. Valid options are \"user\" or \"system\". Defaulting to \"$FLATPAK_DEFAULT_INSTALL_MODE\" install type."
     flatpak_install_mode="$FLATPAK_DEFAULT_INSTALL_MODE"
-    final_dest="$FLATPAK_USER_ROOT/$flatpak_id/current/active/files"
+    final_dest="$FLATPAK_USER_ROOT/app/$flatpak_id/current/$flatpak_version/files"
   fi
 
   log info "Downloading: $final_flatpak_id"
@@ -29,7 +29,7 @@ download() {
   log info "Flatpak Install Mode: $flatpak_install_mode"
 
   download_cmd() {
-    install_flatpak "$flatpak_id" "$flatpak_version" "$flatpak_install_mode"
+    install_flatpak "$flatpak_id" "$flatpak_version" "$flatpak_install_mode" "app" 2>&1
   }
 
   if ! try "$max_retries" "$initial_delay" "$max_delay" download_cmd; then
--- lib/tools/install_flatpak.sh
+++ lib/tools/install_flatpak.sh
@@ -4,20 +4,34 @@ install_flatpak() {
   local flatpak_id="$1"
   local flatpak_version="$2"
   local flatpak_install_mode="$3"
-
-  local final_flatpak_id="$flatpak_id"
+  local flatpak_source_type="$4"
 
   if ! flatpak remote-add --if-not-exists flathub "$FLATHUB_REPO"; then # Ensure flathub is added as a remote
     log error "FlatHub could not be added as a remote"
     return 1
   fi
 
-  if [[ -n "$flatpak_version" ]]; then # If a specific version was given, add it to the Flatpak ID
-    final_flatpak_id="$flatpak_id//$flatpak_version"
+  if [[ ! -n "$flatpak_version" ]]; then # If a specific version was given, add it to the Flatpak ID
+    log error "Flatpak version not defined"
+    return 1
   fi
 
-  if ! flatpak install --"$flatpak_install_mode" -y --or-update --noninteractive flathub "$final_flatpak_id" 2>&1; then
-    log error "Flatpak $flatpak_id could not be installed."
+  if [[ "$flatpak_source_type" == "app" ]]; then
+    flatpak_install_cmd() {
+      flatpak install --"$1" -y --noninteractive flathub "$2"
+      flatpak update --"$1" -y --noninteractive --commit="$3" "$2"
+    }
+  elif [[ "$flatpak_source_type" == "runtime" ]]; then # Is a flatpak runtime
+    flatpak_install_cmd() {
+      flatpak install --"$1" -y --or-update --noninteractive flathub "$2//$3"
+    }
+  else
+    log error "Flatpak source type \"$flatpak_source_type\" is invalid"
+    return 1
+  fi
+
+  if ! flatpak_install_cmd "$flatpak_install_mode" "$flatpak_id" "$flatpak_version"; then
+    log error "Flatpak $flatpak_id version $flatpak_version could not be installed."
     return 1
   fi
 
