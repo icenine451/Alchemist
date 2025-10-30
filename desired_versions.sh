90bed223c245ae43f7470eaee96814bbf9b0d1c0
icenine451 <benjamin.r.shelton@protonmail.com>
2025-10-30 16:59:41 -0400

    Add desired flatpak runtime version placeholder fillers to desired_versions.sh. These placeholders can be used in component_recipe.json files where a specific runtime version is not needed.

--- desired_versions.sh
+++ desired_versions.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Defaults for runtime desired versions
+export DESIRED_QT6_RUNTIME_VERSION="6.10"
+export DESIRED_QT5_RUNTIME_VERSION="5.15-25.08"
+
+# Defaults for component source desired versions
 export AZAHAR_DESIRED_VERSION="2123.3"
 export RUFFLE_DESIRED_VERSION="9555d8fc3d5e5b3c9e9ccea40492277fefb950368f2f2a40a16b69ad42ea3e33"
