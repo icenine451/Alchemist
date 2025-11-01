2e8f005f2cb65481cdb8d72bef89833bdc3bc2ee
IceNine451 <benjamin.r.shelton@protonmail.com>
2025-11-01 15:59:47 -0400

    Simplify "local" downloader plugin, we don't need to be copying local files around, only capturing where they are.

--- lib/downloaders/local.sh
+++ lib/downloaders/local.sh
@@ -5,12 +5,11 @@ downloader_info() {
 }
 
 download() {
-  local file_path="$1"
+  local source="$1"
   local dest="$2"
   local version="$3"
-  local type="$4"
 
-  local final_file_path="$file_path"
+  local final_source="$source"
   local final_dest="$dest"
 
   if [[ ! -d "$dest" ]]; then
@@ -19,35 +18,27 @@ download() {
   fi
 
   # If supplied file name / path contains version placeholders
-  if has_version_placeholder "$file_path"; then
-    final_file_path=$(substitute_version "$file_path" "$version")
+  if has_version_placeholder "$source"; then
+    final_source=$(substitute_version "$source" "$version")
   fi
 
-  if [[ ! "$final_file_path" = /* ]]; then # If provided source path is relative
-    final_file_path="$WORKDIR/$final_file_path"
+  if [[ ! "$final_source" = /* ]]; then # If provided source path is relative
+    final_source="$WORKDIR/$final_source"
   fi
 
   if [[ -d "$dest" ]]; then # If the provided dest is a directory
     log info "Destination $dest is a directory, constructing full destination path..."
-    final_dest="$dest/$(basename $final_file_path)"
+    final_dest="$dest/$(basename $final_source)"
   fi
 
-  if [[ ! -f "$final_file_path" ]]; then
-    log error "Supplied local file could not be found at $final_file_path"
+  if [[ ! -f "$final_source" ]]; then
+    log error "Supplied local file could not be found at $final_source"
     return 1
   fi
 
-  log info "Copying: $final_file_path"
-  log info "Destination: $final_dest"
+  final_dest="$final_source"
 
-  cp "$final_file_path" "$final_dest"
-
-  if [[ ! -f "$final_dest" ]]; then
-    log error "File not copied successfully to destination."
-    return 1
-  fi
-
-  log info "Local file copied successfully"
+  log info "Local file $final_dest validated, proceeding to extraction"
   echo "DOWNLOADED_FILE=$final_dest"
   return 0
 }
